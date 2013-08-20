40ab269ccb1f9103374795214338660284b50288
godfried <pjgjordaan@gmail.com>
2013-08-20 13:19:26 +0200

    ./submit.sh

--- Intlola/src/za/ac/sun/cs/intlola/IntlolaRecord.java
+++ Intlola/src/za/ac/sun/cs/intlola/IntlolaRecord.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class IntlolaRecord extends AbstractHandler {
@@ -11,8 +12,14 @@ public class IntlolaRecord extends AbstractHandler {
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		final IProject project = PluginUtils.getSelectedProject(event);
-		if (!Intlola.getRecordStatus(project)) {
+		if (!Intlola.projectRecording(project) && !Intlola.isRecording()) {
 			Intlola.startRecord(project, HandlerUtil.getActiveShell(event));
+		} else {
+			MessageDialog
+					.openError(
+							HandlerUtil.getActiveShell(event),
+							"Recording Error",
+							"You are already recording a project. Please stop recording it before starting a new recording.");
 		}
 		return null;
 	}
