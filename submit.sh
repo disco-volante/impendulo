8971777964414632eac48bb880c8f3e19b43b8a1
godfried <pjgjordaan@gmail.com>
2013-08-20 14:42:44 +0200

    Only allow for single recording per machine.

--- submit.sh
+++ submit.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
 git add . -A
-<<<<<<< HEAD
 git commit -m "$1"
 git push origin 
-git push backup 
-=======
-git commit -m $0
-git push --set-upstream origin master
-git push --set-upstream backup master
->>>>>>> 40ab269ccb1f9103374795214338660284b50288
+git push backup
